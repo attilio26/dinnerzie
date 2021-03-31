44627e99f3fce9de6932f247d0d9932bc1c4a93f
attilio26 <39518843+attilio26@users.noreply.github.com>
2021-03-31 19:08:02 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -58,10 +58,11 @@ function clean_html_page($str_in){
 	$str_in = str_replace("p'/>"," ",$str_in);
 	$str_in = str_replace("q'/>"," ",$str_in);
 //elimino i caratteri della pagina che non interessano la stazione bedzie
-	$startch = strpos($str_in,"slave1");
-	$str_in = substr($str_in,$startch);
+	//$startch = strpos($str_in,"slave1");
+	//$str_in = substr($str_in,$startch);
 	return $str_in;
 
+	
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
