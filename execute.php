a9fe1c815ab0e2872506e284e664a2de612e250e
attilio26 <39518843+attilio26@users.noreply.github.com>
2021-03-31 19:26:38 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -58,6 +58,7 @@ function clean_html_page($str_in){
 	$str_in = str_replace("o'/>"," ",$str_in);	
 	$str_in = str_replace("p'/>"," ",$str_in);
 	$str_in = str_replace("q'/>"," ",$str_in);
+	$str_in = str_replace("<h2>"," ",$str_in);	
 //elimino i caratteri della pagina che non interessano la stazione bedzie
 	$startch = strpos($str_in,"slave2");
 	$endch = strpos($str_in,"slave1");	
